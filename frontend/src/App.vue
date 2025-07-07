9c5d10542775f34f6bbe824d8dc661cf8324ee13
yiwang0w0 <77381968+yiwang0w0@users.noreply.github.com>
2025-07-07 11:19:06 +0900

    fix: merge duplicate templates in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-<template>
-  <router-view />
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-view />
+  </div>
 </template>
 
 <script>
