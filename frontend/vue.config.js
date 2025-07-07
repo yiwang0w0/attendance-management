22fd43027a8fbac9efab7e49a0e16e58fc3d9437
yiwang0w0 <77381968+yiwang0w0@users.noreply.github.com>
2025-07-07 11:19:01 +0900

    refactor: consolidate vue config export

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-module.exports = {
+  transpileDependencies: true,
   devServer: {
     proxy: {
       '/api': {
@@ -12,4 +11,4 @@ module.exports = {
       }
     }
   }
-}
+})
